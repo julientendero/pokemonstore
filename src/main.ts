ed1274117418725ad1c306433b8b726f40fa28e1
julientendero <julien.tendero@gmail.com>
2021-07-30 13:51:07 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+
+// X-Api-Key
+// pokemon api key: f18e8261-b63b-4b18-af9e-a90c90152d82
